240c071d2fa265ccd4756ee9dd952cf0d556035c
Túlio Mendes <tulioalcan@gmail.com>
2022-08-03 15:27:39 -0300

    requesito 7 ok

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-window.onload = function () {
-  let selected = document.getElementById('container1');
-  selected.classList.add('selected');
+window.onload = function() {
+  function selectedClass() {
+const corPrimaria = document.getElementById('container1');
+corPrimaria.classList.add('selected');
 }
+selectedClass()
+
+const colorPalette = document.getElementById('color-palette');
+function changeColorSelected(color){
+  let colorSelected = document.querySelector('.selected')
+  colorSelected.classList.toggle('selected')
+  color.target.classList.add('selected')
+}
+colorPalette.addEventListener('click', changeColorSelected)
+}
+
