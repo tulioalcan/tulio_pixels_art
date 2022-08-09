3a7d830a28dde1aaf9b85f40ebb9e24456edc3aa
Túlio Mendes <tulioalcan@gmail.com>
2022-08-09 07:28:06 -0300

    Refatorando todo o código para usar mais funções do DOM e fazendo o requesito 9

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
-window.onload = function() {
-  function selectedClass() {
-const corPrimaria = document.getElementById('container1');
+
+function addPalette (){
+  for(let index = 0; index < 4; index += 1){
+  let createPallet = document.createElement('div');
+  createPallet.classList.add('color');
+  let dadPallet = document.getElementById('color-palette');
+  dadPallet.appendChild(createPallet);
+  if (index === 0) {
+    createPallet.id = 'black';
+    createPallet.classList = 'color';
+  } else if (index === 1){
+    createPallet.id = 'blue';
+  } else if (index === 2){
+    createPallet.id = 'green';
+  } else {
+    createPallet.id = 'red';
+  }
+  }
+}
+addPalette();
+
+function createBord (){
+  for(let index = 0; index < 25; index += 1){
+  let createPixelBoard = document.createElement('div');
+  createPixelBoard.classList.add('pixel');
+  let dadPixel = document.getElementById('pixel-board');
+  dadPixel.appendChild(createPixelBoard);
+}
+}
+createBord ();
+
+
+function selectedClass() {
+const corPrimaria = document.getElementById('black');
 corPrimaria.classList.add('selected');
 }
-selectedClass()
+selectedClass();
 
 const colorPalette = document.getElementById('color-palette');
 function changeColorSelected(color){
@@ -22,8 +53,15 @@ function paintPixelsColor(){
       selectedPixel.id = colorSelected.id
     }
   }) 
-}
+ }
 paintPixelsColor();
+
+const clearAll = document.getElementById('clear-board')
+function clear(){
+  const pixels = document.querySelectorAll('.pixel');
+    for(let index = 0; index < pixels.length; index += 1){
+      let clearPixels = pixels[index];
+      clearPixels.style.backgroundColor = 'white';
+    }
 }
-
-
+clearAll.addEventListener('click', clear);
